94e8df70d5daa853090e7a8abf180e2d98e41f7b
Jia-Jingnan <875480307@qq.com>
2021-06-26 00:21:38 +0800

    0626-增加项目管理视图函数和退出登陆视图函数

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.s
@@ -19,9 +20,27 @@ def login(request):
 
         else:
             user = auth.authenticate(username=username, password=password)
+            # context = {'user': user}
             if user is not None:
-                # auth.login(request,user) 记录用户登陆状态
-                return render(request, 'project.html')
-
+                auth.login(request,user) # 记录用户登陆状态
+                # return render(request, 'project.html', context)
+                # response = HttpResponseRedirect('/project_manage/')
+                # response.set_cookie('user', username, 3600) # 添加浏览器cookie
+                request.session['user'] = username
+                return HttpResponseRedirect('/project_manage/')
             else:
-                return render(request, 'login.html', context)
+                return render(request, 'index.html', context)
+
+
+@login_required # 判断用户是否登陆
+def project_manage(request):
+    # username = request.COOKIES.get('user')
+    username = request.session.get('user')
+    context = {'user': username}
+    return render(request, 'project_manage.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    return response
