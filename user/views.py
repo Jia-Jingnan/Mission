15b010247c91eda9fa712244edacb4ad5207239d
Jia-Jingnan <875480307@qq.com>
2021-06-25 23:01:36 +0800

    0625-新增两个视图函数，index和login

--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib import auth
 
-# Create your views here.
+
+# Create your views here.s
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    context = {'error': '用户名或密码错误'}
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        if username == '' or password == '':
+            return render(request, 'login.html', context)
+
+        else:
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                # auth.login(request,user) 记录用户登陆状态
+                return render(request, 'project.html')
+
+            else:
+                return render(request, 'login.html', context)
