a6cf2e7a34cc7f220165ad078d6d0c59c66df89a
Jia-Jingnan <875480307@qq.com>
2021-06-27 01:23:20 +0800

    0627-将project相关视图函数从user app移动到project app下

--- project/views.py
+++ project/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from project.models import Project
 
 # Create your views here.
+
+
+@login_required # 判断用户是否登陆
+def list(request):
+    # username = request.COOKIES.get('user')
+    username = request.session.get('user')
+    # 查询所有项目
+    project_list = Project.objects.all()
+    print(project_list)
+
+    # 查询最近项目
+    project_recent = Project.objects.filter(id__lt=5)
+    print(project_recent)
+
+    # 上下文
+    context = {'user': username, 'projects': project_list, 'project_recent': project_recent}
+    return render(request, 'project_manage.html', context)
+
+
+def add(request):
+    pass
+
+
+def edit(request):
+    pass
+
+
+def delete(request):
+    pass
--- user/views.py
+++ user/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
-from user.models import Project
 
 
 # Create your views here.s
@@ -28,26 +26,12 @@ def login(request):
                 # response = HttpResponseRedirect('/project_manage/')
                 # response.set_cookie('user', username, 3600) # 添加浏览器cookie
                 request.session['user'] = username
-                return HttpResponseRedirect('/project_manage/')
+                return HttpResponseRedirect('/project/list/')
             else:
                 return render(request, 'index.html', context)
 
 
-@login_required # 判断用户是否登陆
-def project_manage(request):
-    # username = request.COOKIES.get('user')
-    username = request.session.get('user')
-    # 查询所有项目
-    project_list = Project.objects.all()
-    print(project_list)
 
-    # 查询最近项目
-    project_recent = Project.objects.filter(id__lt=5)
-    print(project_recent)
-
-    # 上下文
-    context = {'user': username, 'projects': project_list, 'project_recent': project_recent}
-    return render(request, 'project_manage.html', context)
 
 
 def logout(request):
