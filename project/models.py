00d055e537f3d67ac97194f78e639448f810023d
Jia-Jingnan <875480307@qq.com>
2021-06-27 01:22:39 +0800

    0627-将Project 和 Module模型的代码从user app移动到project app下

--- project/models.py
+++ project/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+# 表名 user_project
+class Project(models.Model):
+    # 会自动添加自增长的id
+    name = models.CharField('项目名称', max_length=100, blank=False, default="")
+    describe = models.TextField('项目描述', default='')
+    status = models.BooleanField('项目状态', default=True)
+    create_time = models.DateTimeField('创建时间', auto_now=True)
+
+    # 重写toString方法
+    def __str__(self):
+        return self.name
+
+    # 自定义表名
+    class Meta:
+        db_table = 'project'
+
+
+class Module(models.Model):
+    # 项目模块表
+    # 项目与模块对应关系为一对多
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # 项目删除时，项目下的模块也一起删除
+    name = models.CharField('模块名称', max_length=100, blank=False, default='')
+    describe = models.TextField('模块描述', default='')
+    # 自动添加创建时间
+    create_time = models.DateTimeField('创建时间', auto_now=True)
+
+    # 重写toString方法
+    def __str__(self):
+        return self.name
+
+    # 自定义表名
+    class Meta:
+        db_table = 'module'
