5442f59b465229199cc3c3c1decd1a25184489f5
Jia-Jingnan <875480307@qq.com>
2021-06-27 00:06:38 +0800

    0626-在django后台展示自定义的类

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from user.models import Project, Module
+
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id', 'describe', 'status', 'create_time']
+
+
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id', 'describe', 'project', 'create_time']
+
+# 将自定义的两个类Project和Module也放到django的后台管理，做增加和删除修改等操作
+# 映射到后台,注册到后台
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Module, ModuleAdmin)
+
+
