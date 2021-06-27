8af721f2aa8a11ab8a523e9953f99dfb874a0d53
Jia-Jingnan <875480307@qq.com>
2021-06-27 21:02:43 +0800

    0627-新增module的增删改查路由

--- project/urls.py
+++ project/urls.py
@@ -6,8 +6,16 @@ from project import views
 
 
 urlpatterns = [
+
+    # project urls
     path('list/', views.list),
     path('add/', views.add),
     path('edit/<int:pid>/', views.edit),
-    path('delete/<int:pid>', views.delete),
+    path('delete/<int:pid>/', views.delete),
+
+    # module urls
+    path('module/list/', views.module_list),
+    path('module/add/', views.module_add),
+    path('module/edit/<int:mid>/', views.module_edit),
+    path('module/delete/<int:mid>/', views.module_delete),
 ]
