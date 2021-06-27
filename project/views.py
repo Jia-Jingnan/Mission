85393ce37f7835cd056fad01c5254dcfcc743e74
Jia-Jingnan <875480307@qq.com>
2021-06-27 13:13:13 +0800

    0627-完成添加项目的视图函数，当add方法收到的请求为GET请求时展示项目列表，当请求为POST请求时新增项目

--- project/views.py
+++ project/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from project.models import Project
+from project.form import ProjectForm
 
 # Create your views here.
 
@@ -18,15 +20,48 @@ def list(request):
     print(project_recent)
 
     # 上下文
-    context = {'user': username, 'projects': project_list, 'project_recent': project_recent}
+    context = {
+        'user': username,
+        'projects': project_list,
+        'project_recent': project_recent,
+        'type': 'list',
+    }
     return render(request, 'project_manage.html', context)
 
 
+@login_required # 判断用户是否登陆
 def add(request):
-    pass
+    if request.method == 'GET':
+        form = ProjectForm()
+        # 上下文
+        context = {
+            'type': 'add',
+            'form': form
+        }
+
+    elif request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            # 插入项目信息到project表
+            # 获取表单信息
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+
+            Project.objects.create(name=name, describe=describe)
+            context = {
+                'type': 'add',
+                'form': form
+            }
+            return HttpResponseRedirect('/project/list/', context)
+
+    else:
+        form = ProjectForm()
+
+    return render(request, 'project_manage.html', context)
 
 
 def edit(request):
+    # 获取要编辑的项目的id
     pass
 
 
