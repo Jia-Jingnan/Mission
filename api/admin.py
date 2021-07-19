fdc83ffa7085327f7561879dcab977ce24fcbb45
Jia-Jingnan <875480307@qq.com>
2021-07-19 00:52:43 -0800

    0719-注册到amdin管理后台

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from api.models import Api
+
 
 # Register your models here.
+
+class ApiAdmin(admin.ModelAdmin):
+    list_display = ['project', 'module', 'name', 'url', 'method', 'type', 'header', 'parameter', 'asserts']
+
+
+# 将自定义的两个类Project和Module也放到django的后台管理，做增加和删除修改等操作
+# 映射到后台,注册到后台
+admin.site.register(Api, ApiAdmin)
+
+
